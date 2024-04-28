1066bfc97b40bf9d2b1df96518b0fd04b1beffe5
ThienTho123 <thientho7a4@gmail.com>
2024-04-28 16:37:55 +0700

    Add database connection

---
+++ src/main/java/org/example/SpringbootdemoApplication.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableWebSecurity
+public class SpringbootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemoApplication.class, args);
+	}
+
+}
